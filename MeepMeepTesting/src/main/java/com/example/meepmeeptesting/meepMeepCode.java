03e24cfb16e6c6c19e850a4492dce2e610cb198c
celinayang13 <celinayang.atx@gmail.com>
2024-01-02 11:55:18 -0600

    blue far auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/meepMeepCode.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/meepMeepCode.java
@@ -9,6 +9,8 @@ import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.beans.beancontext.BeanContext;
+
 public class meepMeepCode {
     public static void main(String args[]) {
         MeepMeep meepMeep = new MeepMeep(800);
@@ -16,64 +18,71 @@ public class meepMeepCode {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(60, 60, Math.toRadians(310), Math.toRadians(250), 10.62)
                 .setColorScheme(new ColorSchemeBlueLight())
+                .followTrajectorySequence(drive -> (drive.trajectorySequenceBuilder(new Pose2d(-35, 63, Math.toRadians(0))))
 
-                        .followTrajectorySequence(drive -> (drive.trajectorySequenceBuilder(new Pose2d(54.6, 30, Math.toRadians(0))))
-                                .lineTo(new Vector2d(54, 40.5))
-                                .splineToConstantHeading(new Vector2d(15, 12), Math.toRadians(180))
-                                .addTemporalMarker(1, () -> {})//targetSlidePos = RobotConstants.slideBottom)
-                                .lineTo(new Vector2d(-30, 12))
-                                .splineToConstantHeading(new Vector2d(-55, 11), Math.toRadians(180))
+                        .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
+                        .lineTo(new Vector2d(13, 60))
+                        .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
+                        .waitSeconds(3)
+
+                        .lineTo(new Vector2d(-54, 11))
+                        .splineToConstantHeading(new Vector2d(-30, 12), Math.toRadians(0))
+                        .lineTo(new Vector2d(15, 12))
+                        .addTemporalMarker(1.7, () -> {})//targetSlidePos = RobotConstants.slideLow)
+                        .splineToConstantHeading(new Vector2d(54.6, 30), Math.toRadians(0))
+                        .waitSeconds(5)
+
+                        // place
+                        .lineTo(new Vector2d(-35, 35))
+                        .addSpatialMarker(new Vector2d(-35, 35), () -> {})
+                        .waitSeconds(1)
+                        // goes to backdrop
+                        .lineTo(new Vector2d(-35, 45))
+                        .splineToConstantHeading(new Vector2d(-15, 60), Math.toRadians(0))
+                        .lineTo(new Vector2d(13, 60))
+                        .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
+                        .addSpatialMarker(new Vector2d(54.6, 42), () -> {})
+                        .waitSeconds(.2)
+
+                        //Driving back
+                        .lineTo(new Vector2d(54, 42))
+                        .splineToConstantHeading(new Vector2d(13, 60), Math.toRadians(180))
+                        .lineTo(new Vector2d(-20, 60))
+                        .splineToConstantHeading(new Vector2d(-55, 35.5), Math.toRadians(180))
+                        .waitSeconds(1)
+                        .lineTo(new Vector2d(-54, 35.5))
+                        .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
+                        .lineTo(new Vector2d(13, 60))
+                        .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
+                        .waitSeconds(.2)
+
+                        //Cycle 2
+                        .lineTo(new Vector2d(54, 42))
+                        .splineToConstantHeading(new Vector2d(13, 60), Math.toRadians(180))
+                        .lineTo(new Vector2d(-20, 60))
+                        .splineToConstantHeading(new Vector2d(-55, 35.5), Math.toRadians(180))
+                        .waitSeconds(1)
+                        .lineTo(new Vector2d(-54, 35.5))
+                        .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
+                        .lineTo(new Vector2d(13, 60))
+                        .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
+                        .waitSeconds(.2)
+                        //Cycle 3
+                        .lineTo(new Vector2d(54, 42))
+                        .splineToConstantHeading(new Vector2d(13, 60), Math.toRadians(180))
+                        .lineTo(new Vector2d(-20, 60))
+                        .splineToConstantHeading(new Vector2d(-55, 23.5), Math.toRadians(180))
+                        .waitSeconds(1)
+                        .lineTo(new Vector2d(-54, 23.5))
+                        .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
+                        .lineTo(new Vector2d(13, 60))
+                        .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
+                        .waitSeconds(.2)
+                        .lineTo(new Vector2d(53, 44))
+                        .splineToConstantHeading(new Vector2d(55, 58), Math.toRadians(20))
+                        .build());
 
 
-                                .build());
-
-        /*
-        .lineTo(new Vector2d(-35, 47))
-                                .splineToConstantHeading(new Vector2d(-20,35), Math.toRadians(0))
-                                .lineTo(new Vector2d(-28, 35))
-                                .splineToConstantHeading(new Vector2d(-36, 48), Math.toRadians(90))
-                                .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
-                                .lineTo(new Vector2d(13, 60))
-                                .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
-                                .addSpatialMarker(new Vector2d(54.6, 42), () -> {})
-                                .waitSeconds(.2)
-                                //Driving back
-                                .lineTo(new Vector2d(54, 42))
-                                .splineToConstantHeading(new Vector2d(13, 60), Math.toRadians(180))
-                                .lineTo(new Vector2d(-20, 60))
-                                .splineToConstantHeading(new Vector2d(-55, 35.5), Math.toRadians(180))
-                                .waitSeconds(1)
-                                .lineTo(new Vector2d(-54, 35.5))
-                                .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
-                                .lineTo(new Vector2d(13, 60))
-                                .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
-                                .waitSeconds(.2)
-
-                                //Cycle 2
-                                .lineTo(new Vector2d(54, 42))
-                                .splineToConstantHeading(new Vector2d(13, 60), Math.toRadians(180))
-                                .lineTo(new Vector2d(-20, 60))
-                                .splineToConstantHeading(new Vector2d(-55, 35.5), Math.toRadians(180))
-                                .waitSeconds(1)
-                                .lineTo(new Vector2d(-54, 35.5))
-                                .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
-                                .lineTo(new Vector2d(13, 60))
-                                .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
-                                .waitSeconds(.2)
-                                //Cycle 3
-                                .lineTo(new Vector2d(54, 42))
-                                .splineToConstantHeading(new Vector2d(13, 60), Math.toRadians(180))
-                                .lineTo(new Vector2d(-20, 60))
-                                .splineToConstantHeading(new Vector2d(-55, 23.5), Math.toRadians(180))
-                                .waitSeconds(1)
-                                .lineTo(new Vector2d(-54, 23.5))
-                                .splineToConstantHeading(new Vector2d(-20, 60), Math.toRadians(0))
-                                .lineTo(new Vector2d(13, 60))
-                                .splineToConstantHeading(new Vector2d(54.6, 42), Math.toRadians(0))
-                                .waitSeconds(.2)
-                                .lineTo(new Vector2d(53, 44))
-                                .splineToConstantHeading(new Vector2d(55, 58), Math.toRadians(20))
-         */
 
         /*close 3 cycle auto
         .lineTo(new Vector2d(12, 43))
