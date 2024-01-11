259e45b8c349bb4c744efd76372f518fbf13ccb4
BKthing <3dbkbuilds@gmail.com>
2024-01-11 17:56:31 -0600

    adjusted forward offset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -32,7 +32,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double LATERAL_DISTANCE = 10.11798; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 2.538776; // in; offset of the lateral wheel
+    public static double FORWARD_OFFSET = 2.538776; // in; offset of the lateral wheel 2.538776 1.0625
 
     /*RunLocalizationTest
     *Drag robot along stretch
