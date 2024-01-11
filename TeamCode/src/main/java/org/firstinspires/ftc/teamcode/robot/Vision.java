0212500a2676cd65fe0bb2e2a0b89c63b6efcd25
BKthing <3dbkbuilds@gmail.com>
2024-01-11 17:56:42 -0600

    misc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Vision.java
@@ -28,7 +28,7 @@ public class Vision extends LinearOpMode {
 
             public void onOpened()
             {
-                colorMaskPipeline.setAlliance("Red");
+                colorMaskPipeline.setAlliance("Blue");
                 webcam.setPipeline(colorMaskPipeline);
                 pos = colorMaskPipeline.getPos();
                 System.out.println("Position: " + pos);
