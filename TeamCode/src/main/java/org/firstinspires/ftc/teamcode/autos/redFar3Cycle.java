1d32b33e40624265a5c3840af2d783b98cebde55
enochkonda <enochjadonkonda@gmail.com>
2024-01-24 17:31:38 -0600

    Finished Red Close cycle for all working on red far

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/redFar3Cycle.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.firstinspires.ftc.teamcode.robot.RobotConstants;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+public class redFar3Cycle {
+}
+
