f27da9414df52af61f939b2d4ce448557c4e9798
BKthing <3dbkbuilds@gmail.com>
2024-02-09 15:19:44 -0600

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/TestingForwards.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/TestingForwards.java
@@ -4,11 +4,12 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.robot.HwMap;
 
-@Autonomous
+@TeleOp
 public class TestingForwards extends LinearOpMode {
 
     @Override
